5b1aa761d1bd2a07f170360f1f56a2d65911aa84
Deo_Favente <56390262+Deo-Favente@users.noreply.github.com>
2026-01-19 23:43:11 +0100

    add ios support

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.storekit.fr',
+  appName: 'storekit',
+  webDir: 'dist/storekit/browser',
+};
+
+export default config;
